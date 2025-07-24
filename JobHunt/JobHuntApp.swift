7aa8a5f160369c70bc10d93101a0c3d033ae77e0
------- <tamarjargal25@my.whitworth.edu>
2025-07-24 00:30:25 -0700

    Initial Commit

---
+++ JobHunt/JobHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobHuntApp.swift
+//  JobHunt
+//
+//  Created by temuulen on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct JobHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JobHuntTests/JobHuntTests.swift
@@ -0,0 +1,17 @@
+//
+//  JobHuntTests.swift
+//  JobHuntTests
+//
+//  Created by temuulen on 7/24/25.
+//
+
+import Testing
+@testable import JobHunt
+
+struct JobHuntTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
